246cc7b860434d00725c840e4b0b50c178b8b0ad
longpt99 <longpt@vmodev.com>
2022-06-15 16:55:52 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { AppConst } from './app.const';
 import './App.css';
 import Header from './components/Header/Header';
 import User from './components/User/User';
+import AuthImage from './images/index';
 
 function App() {
   const [data, setData] = useState({
-    client_id: 'WdS95W60BSOLdS_m0v1UO',
-    client_secret:
-      '_YtWUOSGv7zbVy5BgWiY-aZvnSrCjqfjGLJvaL7oIBMfEgpqsvk-KvUBEGQG7J1au7ukHQcTbFjrIJ9RLn63ng',
+    client_id: '',
+    client_secret: '',
     scope: 'openid',
     response_type: 'code',
-    redirect_uri: `${window.location.protocol}//${window.location.host}/user`,
-    logout_redirect_uri: `${window.location.protocol}//${window.location.host}`,
+    redirect_uri: '',
+    logout_redirect_uri: '',
   });
   const [token, setToken] = useState(() => {
     const token = localStorage.getItem('token');
@@ -50,82 +50,104 @@ function App() {
       ) : (
         <div className="login-container">
           <div className="login-card">
-            <h1>OIDC Client</h1>
-            <form
-              className="form"
-              onSubmit={(e) => {
-                e.preventDefault();
-              }}
-            >
-              <label>Client Id</label>
-              <input
-                required
-                placeholder="Eg: cg219l13GsIcLvGUY-hJP"
-                value={data.client_id}
-                onChange={(e) => {
-                  setData({ ...data, client_id: e.target.value });
+            <div className="bodyTop">
+              <a
+                className="logoButton"
+                href="https://www.dentity.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={AuthImage.logoBlueWeb}
+                  alt={'dentity'}
+                  className="logo"
+                />
+              </a>
+            </div>
+            <div className="body">
+              <div className="titleOIDC">Configure OIDC Client</div>
+              <form
+                className="form"
+                onSubmit={(e) => {
+                  e.preventDefault();
                 }}
-              />
-              <label>Client Secret</label>
-              <input
-                required
-                placeholder="Eg: cg219l13GsIcLvGUY-hJPsdfsdafdafJKGHHJGVBJHGBhshjdgahjsgdjhas"
-                value={data.client_secret}
-                onChange={(e) => {
-                  setData({ ...data, client_secret: e.target.value });
-                }}
-              />
-              <input
-                required
-                placeholder="Eg: code"
-                value={data.response_type}
-                onChange={(e) => {
-                  setData({ ...data, response_type: e.target.value });
-                }}
-                hidden
-              />
-              <label>Redirect Link</label>
-              <input
-                required
-                placeholder="Eg: http://localhost:3000/user"
-                value={data.redirect_uri}
-                onChange={(e) => {
-                  setData({ ...data, redirect_uri: e.target.value });
-                }}
-              />
-              <label>Logout Link</label>
-              <input
-                required
-                placeholder="Eg: http://localhost:3000"
-                value={data.logout_redirect_uri}
-                onChange={(e) => {
-                  setData({ ...data, logout_redirect_uri: e.target.value });
-                }}
-              />
-              <input
-                required
-                value={data.scope}
-                placeholder="Eg: openid"
-                onChange={(e) => {
-                  setData({ ...data, scope: e.target.value });
-                }}
-                hidden
-              />
-              <div style={{ display: 'flex' }}>
-                <button
-                  class="btn-submit btn-logout"
-                  onClick={() => handleSubmit(logoutLink)}
-                >
-                  Logout
-                </button>
-                <button
-                  class="btn-submit"
-                  onClick={() => handleSubmit(loginLink)}
-                >
-                  Grant Access
-                </button>
-              </div>
-            </form>
+              >
+                <label className="label">Client ID</label>
+                <input
+                  required
+                  placeholder="IbTmT2-URGaDIGgnI4vMh"
+                  value={data.client_id}
+                  onChange={(e) => {
+                    setData({ ...data, client_id: e.target.value });
+                  }}
+                  className="input"
+                />
+                <label className="label">Client Secret</label>
+                <input
+                  required
+                  placeholder="EoUHG3M3Eycg-9snrnlF9a9SDVrR1F2aPXsLIK9kE4dkZDbbT2MAX24hvHTafAEztD86fHoihBiVF6Tja2DUcg"
+                  value={data.client_secret}
+                  onChange={(e) => {
+                    setData({ ...data, client_secret: e.target.value });
+                  }}
+                  className="input"
+                />
+                <input
+                  required
+                  placeholder="Eg: code"
+                  value={data.response_type}
+                  onChange={(e) => {
+                    setData({ ...data, response_type: e.target.value });
+                  }}
+                  className="inputNone"
+                  hidden
+                />
+                <label className="label">Redirect Link</label>
+                <input
+                  className="input"
+                  required
+                  placeholder={`${window.location.protocol}//${window.location.host}/user`}
+                  value={data.redirect_uri}
+                  onChange={(e) => {
+                    setData({ ...data, redirect_uri: e.target.value });
+                  }}
+                />
+                <label className="label">Logout Link</label>
+                <input
+                  className="input"
+                  required
+                  placeholder={`${window.location.protocol}//${window.location.host}`}
+                  value={data.logout_redirect_uri}
+                  onChange={(e) => {
+                    setData({ ...data, logout_redirect_uri: e.target.value });
+                  }}
+                />
+                <input
+                  required
+                  value={data.scope}
+                  placeholder="Eg: openid"
+                  onChange={(e) => {
+                    setData({ ...data, scope: e.target.value });
+                  }}
+                  className="inputNone"
+                  hidden
+                />
+                <div className="container_btn" style={{ display: 'flex' }}>
+                  <button
+                    class="btn-submit btn-logout"
+                    onClick={() => handleSubmit(logoutLink)}
+                  >
+                    Logout
+                  </button>
+                  <button
+                    class="btn-submit"
+                    onClick={() => handleSubmit(loginLink)}
+                  >
+                    Grant Access
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       )}
---
+++ src/images/index.js
@@ -0,0 +1,5 @@
+import logoBlueWeb from './ic_logo_blue_web.svg';
+
+export default {
+  logoBlueWeb,
+};
